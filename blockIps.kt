eee4a9c181712bbfba647760a7ce3f5543e71b12
Daniel Levi <daniellevi11234567@gmail.com>
2020-11-26 10:02:33 +0200

    added genericIsAllowed func
    implement genericIsAllowed func

--- blockIps.kt
+++ blockIps.kt
@@ -1,4 +1,4 @@
-class TreeNode(
+public class TreeNode(
     var left1: TreeNode? = null,
     var right1: TreeNode? = null,
     var blocked: Boolean? = false
@@ -6,10 +6,12 @@ class TreeNode(
 
 class IpTree(private val root: TreeNode){
 
-    fun createTree(ip: String, node:TreeNode){
+    // Employing a binary tree data structure in order to store the CIDR blocked network address
+    // Time complexity: O(log(n))
+    public fun createTree(ip: String, node:TreeNode){
         var currNode = node;
-        for(element in ip){
-            if (element == '0'){
+        for(bit in ip){
+            if (bit == '0'){
                 if (currNode.right1 != null) {
                     currNode = currNode.right1!!
                 }
@@ -19,7 +21,7 @@ class IpTree(private val root: TreeNode){
                     currNode = newNode
                 }
             }
-            if (element == '1'){
+            if (bit == '1'){
                 if(currNode.left1 != null){
                     currNode = currNode.left1!!
                 }
@@ -33,34 +35,31 @@ class IpTree(private val root: TreeNode){
         currNode.blocked = true
     }
 
-    fun checkIpValidation(curr_ip: String):Boolean{
+    // Given an IP, we determine if it's blocked by traveling down the binary tree.
+    // Time complexity: O(log(n)), n = |ip_address_binary_length|
+    public fun validationIps(curr_ip: String):Boolean{
         var currNode = this.root
         for (i in 0..curr_ip.length){
             val bit = curr_ip[i]
             if(currNode.right1 != null){
-                if(currNode.right1!!.blocked!!){ return true }
+                if(currNode.right1!!.blocked!!){ return false }
             }
             if(currNode.left1 != null){
-                if(currNode.left1!!.blocked!!){ return true }
+                if(currNode.left1!!.blocked!!){ return false }
             }
 
             if (bit == '0'){
-                if(currNode.right1 == null){ return false }
-                else{
-                    currNode = currNode.right1!!
-                }
+                if (currNode.right1 != null) currNode = currNode.right1!!
+                else { return true }
             }
             else if(bit == '1') {
-                if (currNode.left1 == null) { return false}
-                else {
-                    currNode = currNode.left1!!
-                }
+                if (currNode.left1 != null) currNode = currNode.left1!!
+                else { return true}
             }
         }
-        return false
+        return true
     }
 }
-
 
 fun getBinaryNumber(decimalNumber: Int): String {
     var decimalNumber = decimalNumber
@@ -71,7 +70,6 @@ fun getBinaryNumber(decimalNumber: Int): String {
         decimalNumber /= 2
         binaryStr.append(r)
     }
-
     return binaryStr.reverse().toString()
 }
 
@@ -99,41 +97,65 @@ fun createIpFromCidr(cidr_ip: String): String{
     return binIp
 }
 
-
-fun isAllowed(incomingIp: String): Boolean{
-    val arrayOfCidrIp = arrayOf("192.3.4.92/8", "192.3.77.92/16", "192.3.32.92/24")
-    val map = arrayOfNulls<String>(arrayOfCidrIp.size)
+public fun genericIsAllowed(incomingIpsArray: Array<String>, cidrIpsToBlock: Array<String>){
+    var arrayOfCidrIp =  cidrIpsToBlock
+    val filterdIps = arrayOfNulls<String>(arrayOfCidrIp.size)
     val root = TreeNode()
     val ipTree = IpTree(root)
 
     var i = 0
-    for (element in arrayOfCidrIp) {
-        map[i] = createIpFromCidr(element)
+    for (cidr in arrayOfCidrIp) {
+        filterdIps[i] = createIpFromCidr(cidr)
         i += 1
     }
 
-    for (ip_idx in 0 until map.size){
-        val cidrIp = map[ip_idx]
+    for (ip_idx in 0 until filterdIps.size){
+        val cidrIp = filterdIps[ip_idx]
+        if (cidrIp != null) ipTree.createTree(cidrIp, root)
+    }
+    for(incomingIp in incomingIpsArray) {
+        val test = convertIpToBin(incomingIp)
+        val result = ipTree.validationIps(test)
+        if (result) print("Ip is valid\n") else print("Ip is not valid\n")
+    }
+}
+
+public fun isAllowed(incomingIp: String): Boolean{
+    val arrayOfCidrIp = arrayOf("192.3.4.92/8", "192.3.77.92/16", "192.3.32.92/24")
+    val filterdIps = arrayOfNulls<String>(arrayOfCidrIp.size)
+    val root = TreeNode()
+    val ipTree = IpTree(root)
+
+    var i = 0
+    for (cidr in arrayOfCidrIp) {
+        filterdIps[i] = createIpFromCidr(cidr)
+        i += 1
+    }
+
+    for (ip_idx in 0 until filterdIps.size){
+        val cidrIp = filterdIps[ip_idx]
         if (cidrIp != null) {
             ipTree.createTree(cidrIp, root)
         }
     }
 
     val test = convertIpToBin(incomingIp)
-    val result = ipTree.checkIpValidation(test)
+    val result = ipTree.validationIps(test)
     return if (result) {
-        print("Ip is not valid\n")
-        false
-    } else {
         print("Ip is valid\n")
         true
+    } else {
+        print("Ip is not valid\n")
+        false
     }
 }
 
 
-
 fun main(args: Array<String>) {
-    print(isAllowed("192.40.30.3"))
+//    print(isAllowed("192.40.30.3"))
 //    print(isAllowed("191.3.4.0"))
+    val arrayOfCidrIp = arrayOf("192.3.4.92/31", "192.3.76.92/24", "192.3.32.92/24", "35.3.32.92/8")
+    val incomingIpsArray = arrayOf("192.40.30.3", "191.40.30.3", "35.6.222.121", "192.3.87.92")
+    genericIsAllowed(incomingIpsArray, arrayOfCidrIp)
 }
 
